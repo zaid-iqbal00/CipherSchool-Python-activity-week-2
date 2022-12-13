ee6c06f8bc27ae34b493155599a780aa0f9e3b3c
zaid iqbal <115379034+zaid-iqbal00@users.noreply.github.com>
2022-12-13 21:17:04 +0530

    Update classs-122.py

--- week 2 python/classs-122.py
+++ week 2 python/classs-122.py
@@ -1,6 +1,6 @@
-user={'name': 'Aaryan' , 'age': '18'}
+user={'name': 'zaid' , 'age': '18'}
 print(user.get('names','not found!'))
 
 
-user={'name': 'Aaryan' , 'age': '18', 'age': '20'}
+user={'name': 'zaid' , 'age': '18', 'age': '20'}
 print(user.get('age'))
