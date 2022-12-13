efc9727f4e19647075e8625b3c9f2107f39bdf0e
zaid iqbal <115379034+zaid-iqbal00@users.noreply.github.com>
2022-12-13 21:14:47 +0530

    Add files via upload

---
+++ week 2 python/class-120.py
@@ -0,0 +1,7 @@
+user_info= {
+    'name': 'aaryan',
+    'age' : '18',
+}
+more_info={'name' : 'Aaryan Kashyap' , 'state' : 'Haryana' , 'Hobbies' : ['coding' , 'reading']}
+user_info.update(more_info)
+print(user_info)
---
+++ week 2 python/class-92.py
@@ -0,0 +1,12 @@
+# split method
+user_info='harshit 24'.split()
+print(user_info)
+
+name,age='aaryan,18'.split(",")
+print(name)
+print(age)
+
+
+#joint method
+user_info=['aaryan','18']
+print(','.join(user_info))
